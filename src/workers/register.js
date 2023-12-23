8ce43e2d8adf00b493f43d6245d9a4d51a4a80a6
TriremeSolutions <themellowread@gmail.com>
2023-12-23 06:08:14 +0100

    build not the issue for unincluded serviceworker file; modified register script itself to include worker

--- src/workers/register.js
+++ src/workers/register.js
@@ -1,3 +1,36 @@
+const sw=()=>{
+    const statCache = 'static-assets-v1';
+    self.addEventListener('install',(e)=>{
+        e.waitUntil(
+            caches.open(statCache)
+            .then(
+                (cache) => {
+                    return fetch('./manif.json')
+                    .then(resp => resp.json())
+                    .then(staticPaths => cache.addAll(staticPaths));
+                }
+            )
+        );
+    });
+
+
+    self.addEventListener('activate', (ev)=>{
+        ev.waitUntil(self.clients.claim());
+    });
+
+    self.addEventListener('fetch', (e)=>{
+        e.respondWith(
+            caches.match(e.request)
+            .then(
+                (response) => {
+                    return response ||
+                    fetch(e.request);
+                }
+            )
+        );
+    });
+}
+
 export function registerSW (){
     if ('serviceWorker' in navigator) {
         window.addEventListener('load',()=>{
