59abf55eeaa837c9f9130e4ebf4f8c73604d699d
TriremeSolutions <themellowread@gmail.com>
2023-12-23 15:30:08 +0100

    added service worker script inline html

---
+++ build/register.js
@@ -0,0 +1,19 @@
+function registerSW (){
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load',()=>{
+            navigator.serviceWorker.register('./service-worker.js')
+            .then(
+                registration => {
+                    console.log('SW registered with scope:', registration.scope);
+                }
+            )
+            .catch(
+                (err)=>{
+                    console.error('SW registration failed: ', err);
+                }
+            );
+        });
+    }            
+}
+
+registerSW();
